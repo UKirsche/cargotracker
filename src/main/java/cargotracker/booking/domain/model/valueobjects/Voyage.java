06c15a50e541bc26b5a0199ff45b06ccc986cff7
Uwe Kirschenmann <ukirschenmann@gmx.de>
2022-12-27 10:42:39 +0100

    Weiter mit Boilerplate-Code

--- src/main/java/cargotracker/booking/domain/model/valueobjects/Voyage.java
+++ src/main/java/cargotracker/booking/domain/model/valueobjects/Voyage.java
@@ -2,23 +2,24 @@ package cargotracker.booking.domain.model.valueobjects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  *  Class representing the Cargo Voyage
  */
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
 public class Voyage {
 
     @Column(name = "voyage_number")
     private String voyageNumber;
 
-    public Voyage(){}
-
     public Voyage(String voyageId){
         this.voyageNumber = voyageNumber;
     }
 
-    public String getVoyageId(){return this.voyageNumber;}
-
-    public void setVoyageId(String voyageId){this.voyageNumber = voyageNumber;}
 }
